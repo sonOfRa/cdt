2e4bbce267d6e53db62cc615db4d98de43aec105
Simon Levermann <simon@slevermann.de>
2015-01-30 16:52:14 +0100

    Fill in test stubs for unary operators

--- core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/refactoring/encapsulatefield/EncapsulateFieldRefactoringTestUnaryOperators.java
+++ core/org.eclipse.cdt.ui.tests/ui/org/eclipse/cdt/ui/tests/refactoring/encapsulatefield/EncapsulateFieldRefactoringTestUnaryOperators.java
@@ -17,50 +17,466 @@ public class EncapsulateFieldRefactoringTestUnaryOperators extends
 		super(name);
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// foo++;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// setFoo(getFoo() + 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 0;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testBasicPostIncrement() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// ++foo;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// setFoo(getFoo() + 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 0;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testBasicPreIncrement() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// foo--;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// setFoo(getFoo() - 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 0;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testBasicPostDecrement() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// --foo;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// setFoo(getFoo() - 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 0;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testBasicPreDecrement() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[foo++];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 2;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[getFoo()];
+	// setFoo(getFoo() + 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 2;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testPostIncrementUsage() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[++foo];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 2;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// setFoo(getFoo() + 1);
+	// int bar = array[getFoo()];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 2;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testPreIncrementUsage() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[foo--];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 2;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[getFoo()];
+	// setFoo(getFoo() - 1);
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 2;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testPostDecrementUsage() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// int bar = array[--foo];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 2;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
+	// ====================
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int array[5] = { 1, 2, 3, 4, 5 };
+	// setFoo(getFoo() - 1);
+	// int bar = array[getFoo()];
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// int getFoo() const {
+	// return foo;
+	// }
+	//
+	// void setFoo(int foo_) {
+	// foo = foo_;
+	// }
+	// private:
+	// int foo = 2;
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testPreDecrementUsage() throws Exception {
 		assertRefactoringSuccess();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int bar = foo++ + foo++;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testUnsequencedPostIncrement() throws Exception {
 		assertRefactoringFailure();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int bar = ++foo + ++foo;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testUnsequencedPreIncrement() throws Exception {
 		assertRefactoringFailure();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int bar = foo-- + foo--;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testUnsequencedPostDecrement() throws Exception {
 		assertRefactoringFailure();
 	}
 
+	// A.h
+	// #ifndef A_H_
+	// #define A_H_
+	//
+	// class A {
+	// public:
+	// A() {
+	// int bar = --foo + --foo;
+	// }
+	//
+	// virtual ~A() {
+	// }
+	//
+	// /*$*/int foo = 0;/*$$*/
+	// };
+	//
+	// #endif /*A_H_*/
 	public void testUnsequencedPreDecrement() throws Exception {
 		assertRefactoringFailure();
 	}
